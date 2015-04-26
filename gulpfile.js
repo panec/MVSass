2eb8d386af78e4ca2fd8f9c1fe6100cdf69466a7
Maciej Panecki <maciej.panecki@gmail.com>
2015-04-26 23:28:34 +0100

    Updated examples

--- gulpfile.js
+++ gulpfile.js
@@ -4,62 +4,93 @@ var gulp = require('gulp'),
 	$    = require('gulp-load-plugins')();
 
 gulp.task('clean', function () {
+	gulp.src('examples/**/css', { read: false })
+		.pipe($.clean());
+
 	return gulp.src(['dist/'], { read: false })
-	.pipe($.clean());
+		.pipe($.clean());
 });
 
-gulp.task('styles', function () {
-	gulp.src('examples/theme/css/**/*.css', { read: false })
-	.pipe($.clean());
 
-	gulp.src('examples/theme/sass/**/*.scss')
-	.pipe($.compass({
-		time        : true,
-		comments    : false,
-		style       : 'nested',
-		config_file : 'examples/theme/config.rb',
-		css         : 'examples/theme/css',
-		sass        : 'examples/theme/sass',
-		require     : './src/ruby/parentsSelector.rb'
-	}))
-	.pipe($.duration('Compile time'))
-	.pipe($.size({ title: 'Example css before optimization' }))
-	.pipe($.rename({
-		suffix: "-opt"
-	}))
-	.pipe($.combineMediaQueries())
-	.pipe($.csso())
-	.pipe($.duration('Optimization time'))
-	.pipe($.size({ title: 'Example css after optimization' }))
-	.pipe(gulp.dest('examples/theme/css'))
-	.pipe($.rename({
-		suffix: "-beauty"
-	}))
-	.pipe($.jsbeautifier({
-		logSuccess: false,
-		indentChar: "	",
-		indentSize: 1
-	}))
-	.pipe($.duration('Beautifing time'))
-	.pipe($.size({ title: 'Example css after beautifing' }))
-	.pipe(gulp.dest('examples/theme/css'));
-});
 
-gulp.task('default', ['clean'], function () {
+
+
+gulp.task('compileLib', ['clean'], function () {
 	gulp.src(["src/**/*.scss", "!src/_mvs.scss"])
-	.pipe($.order([
-		"_variables.scss",
-		"functions/_number.scss",
-		"functions/_string.scss",
-		"functions/_list.scss",
-		"functions/_map.scss",
-		"functions/_mvs.scss"
-		]))
-	.pipe($.concat("mvsass.scss"))
-	.pipe(gulp.dest("dist/"))
-	.pipe($.size({ title: 'Combined mvsass.scss' }));
+		.pipe($.order([
+			"_variables.scss",
+			"functions/_number.scss",
+			"functions/_string.scss",
+			"functions/_list.scss",
+			"functions/_map.scss",
+			"functions/_mvs.scss"
+			]))
+		.pipe($.concat("mvsass.scss"))
+		.pipe(gulp.dest("dist/"))
+		.pipe($.size({ title: 'Combined mvsass.scss' }));
 
 	gulp.src(["src/ruby/*.rb"])
-	.pipe(gulp.dest("dist/"))
-	.pipe($.size());
+		.pipe(gulp.dest("dist/"))
+		.pipe($.size());
 });
+
+gulp.task('cleanLib', function () {
+	return gulp.src('dist/', { read: false })
+		.pipe($.clean());
+});
+
+gulp.task('compileExample', function () {
+	gulp.src('examples/base/sass/**/*.scss')
+		.pipe($.compass({
+			time        : true,
+			comments    : false,
+			style       : 'nested',
+			import_path : 'examples/_core/sass',
+			// config_file : 'examples/base/config.rb'
+			css         : 'examples/base/css',
+			sass        : 'examples/base/sass',
+			require     : './src/ruby/parentsSelector.rb'
+		}))
+		.pipe($.duration('Compile time'))
+		.pipe($.size({ title: 'Example css before optimization' }))
+		.pipe($.rename({
+			suffix: "-opt"
+		}))
+		.pipe($.combineMediaQueries())
+		.pipe($.csso())
+		.pipe($.duration('Optimization time'))
+		.pipe($.size({ title: 'Example css after optimization' }))
+		.pipe(gulp.dest('examples/base/css'))
+		.pipe($.rename({
+			suffix: "-beauty"
+		}))
+		.pipe($.jsbeautifier({
+			logSuccess: false,
+			indentChar: "	",
+			indentSize: 1
+		}))
+		.pipe($.duration('Beautifing time'))
+		.pipe($.size({ title: 'Example css after beautifing' }))
+		.pipe(gulp.dest('examples/base/css'));
+});
+
+gulp.task('watchExample', function () {
+	return gulp.watch('examples/base/sass/**/*.scss', ['compileExample']);
+});
+
+gulp.task('cleanExample', function () {
+	return gulp.src('examples/**/css', { read: false })
+		.pipe($.clean());
+});
+
+gulp.task('hostExample', function() {
+	gulp.src('examples')
+		.pipe($.webserver({
+			// livereload: true,
+			directoryListing: true,
+			open: "html/index.html"
+		}));
+});
+
+gulp.task('default', [ 'cleanLib', 'compileLib' ]);
+gulp.task('example', [ 'cleanExample', 'compileExample', 'watchExample', 'hostExample' ]);
